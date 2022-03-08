72e42e879358a4436f617626878455ac48160868
ycshang <351234359@qq.com>
2022-03-08 20:56:22 +0800

    修改配置类

--- src/main/java/com/ycshang/boot/starter/config/Md5AutoConfiguration.java
+++ src/main/java/com/ycshang/boot/starter/config/Md5AutoConfiguration.java
@@ -1,6 +1,8 @@
 package com.ycshang.boot.starter.config;
 
 import com.ycshang.boot.starter.service.Md5Service;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,9 @@ import org.springframework.context.annotation.Configuration;
  * @author: ycshang
  * @create: 2022-03-08 20:15
  **/
+
 @Configuration
+@EnableConfigurationProperties({RabbitProperties.class})
 public class Md5AutoConfiguration {
     @Bean
     Md5Service md5Service() {
